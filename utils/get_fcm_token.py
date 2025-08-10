29417d199f34bc6dfff2fa96c57a52d0fc2db062
Yash <yashkantsingh3@gmail.com>
2025-08-10 14:07:50 +0530

    added fcm push notiifcation changes

---
+++ utils/get_fcm_token.py
@@ -0,0 +1,15 @@
+import json
+import google.auth
+from google.oauth2 import service_account
+import google.auth.transport.requests
+
+def get_access_token():
+    credentials = service_account.Credentials.from_service_account_file(
+        r"C:\Users\YashKantSingh\Desktop\storzee-backend\storzee-backend\storezee-b5f0e-firebase-adminsdk-fbsvc-0c43f4140f.json",
+        scopes=["https://www.googleapis.com/auth/firebase.messaging"]
+    )
+    request = google.auth.transport.requests.Request()
+    credentials.refresh(request)
+    return credentials.token
+
+print(get_access_token())
---
+++ utils/send_fcm_notification.py
@@ -0,0 +1,33 @@
+import requests
+from utils.get_fcm_token import get_access_token
+
+def send_fcm_v1_message(token, title, body, data=None):
+    project_id = "storezee-b5f0e"  # from Firebase project settings
+    url = f"https://fcm.googleapis.com/v1/projects/{project_id}/messages:send"
+    access_token = get_access_token()
+
+    headers = {
+        "Authorization": f"Bearer {access_token}",
+        "Content-Type": "application/json; UTF-8",
+    }
+
+    payload = {
+        "message": {
+            "token": token,
+            "notification": {
+                "title": title,
+                "body": body
+            },
+            "data": data or {}
+        }
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    return response.json()
+
+notification = send_fcm_v1_message(token='eGYqgJ6EQ4uodkhLkqFdx5:APA91bHCd3X0eAnenCK-6EtLPfIFAjxixIG6-VmbJZvS3w-y1kQX_M3a9xaJ-I52lvTjws-FxKYvKwXQBcYxqvEPiPabADqduUglkZCY4TwLcvy95hQdff0',
+                                   title='Booking Confirmed',
+                                   body='Your luggage pickup is scheduled.',
+                                   data={"booking_id": "123456"})
+
+print(notification)
